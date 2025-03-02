43a47316485d88bc125dc27ccefa4cbadf1ac1e3
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-03-02 12:35:09 +0100

    WIP

--- src/language_servers/language_server.rs
+++ src/language_servers/language_server.rs
@@ -6,6 +6,7 @@ use zed_extension_api::{
 pub struct LanguageServerBinary {
     pub path: String,
     pub args: Option<Vec<String>>,
+    pub env: Option<Vec<(String, String)>>,
 }
 
 pub trait LanguageServer {
@@ -31,7 +32,7 @@ pub trait LanguageServer {
         Ok(zed::Command {
             command: binary.path,
             args: binary.args.unwrap_or(Self::get_executable_args()),
-            env: Default::default(),
+            env: binary.env.unwrap_or_default(),
         })
     }
 
@@ -40,12 +41,6 @@ pub trait LanguageServer {
         language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<LanguageServerBinary> {
-        let output = Command::new("ls").output()?;
-
-        dbg!(&output.status);
-        dbg!(String::from_utf8_lossy(&output.stdout).to_string());
-        dbg!(String::from_utf8_lossy(&output.stderr).to_string());
-
         let lsp_settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)?;
 
         if let Some(binary_settings) = lsp_settings.binary {
@@ -53,6 +48,7 @@ pub trait LanguageServer {
                 return Ok(LanguageServerBinary {
                     path,
                     args: binary_settings.arguments,
+                    env: Default::default(),
                 });
             }
         }
@@ -75,16 +71,58 @@ pub trait LanguageServer {
                         ]
                         .concat(),
                     ),
+                    env: Default::default(),
                 })
                 .ok_or_else(|| "Unable to find the 'bundle' command.".into())
         } else {
-            worktree
-                .which(Self::EXECUTABLE_NAME)
-                .map(|path| LanguageServerBinary {
-                    path,
+            let gem_home = std::env::current_dir()
+                .map_err(|e| format!("Failed to get current directory: {}", e))?
+                .to_string_lossy()
+                .to_string();
+
+            // Check if the gem is already installed
+            if let Some(existing_binary) = worktree.which(Self::EXECUTABLE_NAME) {
+                return Ok(LanguageServerBinary {
+                    path: existing_binary,
                     args: Some(Self::get_executable_args()),
-                })
-                .ok_or_else(|| format!("Unable to find the '{}' command.", Self::EXECUTABLE_NAME))
+                    env: Some(vec![(
+                        "GEM_PATH".to_string(),
+                        format!("{}:$GEM_PATH", gem_home),
+                    )]),
+                });
+            }
+
+            let output = Command::new("gem")
+                .env("GEM_HOME", gem_home.clone())
+                .arg("install")
+                .arg("--no-user-install")
+                .arg("--no-format-executable")
+                .arg("--no-document")
+                .arg(Self::GEM_NAME)
+                .output()?;
+
+            let stderr_output = String::from_utf8_lossy(&output.stderr).to_string();
+
+            match output.status {
+                Some(0) => Ok(LanguageServerBinary {
+                    path: format!("{}/bin/{}", gem_home, Self::EXECUTABLE_NAME),
+                    args: Some(Self::get_executable_args()),
+                    env: Some(vec![(
+                        "GEM_PATH".to_string(),
+                        format!("{}:$GEM_PATH", gem_home),
+                    )]),
+                }),
+                Some(status) => Err(format!(
+                    "Failed to install {} gem (status: {})\nError: {}",
+                    Self::GEM_NAME,
+                    status,
+                    stderr_output
+                )),
+                None => Err(format!(
+                    "Failed to start language server: {}",
+                    stderr_output
+                )),
+            }
         }
     }
 }
