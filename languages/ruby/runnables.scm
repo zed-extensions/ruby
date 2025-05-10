6bbadba19af7e56dcd02ef5ddf9e0bba71be56e0
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-05-10 09:06:48 +0200

    Update EditorConfig config (#74)
    Include settings for
    
    - Rust files
    - TOML
    - Change SCM indentation from 4 to 2 to match tree-sitter

--- languages/ruby/runnables.scm
+++ languages/ruby/runnables.scm
@@ -4,55 +4,55 @@
 
 ; Tests that inherit from a specific class
 (
-    (class
-        name: [
-          (constant) @run
-          (scope_resolution scope: (constant) name: (constant) @run)
-        ]
-        (superclass (scope_resolution) @superclass (#match? @superclass "(::IntegrationTest|::TestCase|::SystemTestCase|Minitest::Test|TLDR)$"))
-    ) @_ruby-test
-    (#set! tag ruby-test)
+  (class
+    name: [
+      (constant) @run
+      (scope_resolution scope: (constant) name: (constant) @run)
+    ]
+    (superclass (scope_resolution) @superclass (#match? @superclass "(::IntegrationTest|::TestCase|::SystemTestCase|Minitest::Test|TLDR)$"))
+  ) @_ruby-test
+  (#set! tag ruby-test)
 )
 
 (
-    (call
-        method: (identifier) @run (#eq? @run "test")
-        arguments: (argument_list (string (string_content) @_name))
-    ) @_ruby-test
-    (#set! tag ruby-test)
+  (call
+    method: (identifier) @run (#eq? @run "test")
+    arguments: (argument_list (string (string_content) @_name))
+  ) @_ruby-test
+  (#set! tag ruby-test)
 )
 
 ; Methods that begin with test_
 (
-    (method
-        name: (identifier) @run (#match? @run "^test_")
-    ) @_ruby-test
-    (#set! tag ruby-test)
+  (method
+    name: (identifier) @run (#match? @run "^test_")
+  ) @_ruby-test
+  (#set! tag ruby-test)
 )
 
 ; System tests that inherit from ApplicationSystemTestCase
 (
-    (class
-        name: (constant) @run (superclass) @superclass (#match? @superclass "(ApplicationSystemTestCase)$")
-    ) @_ruby-test
-    (#set! tag ruby-test)
+  (class
+    name: (constant) @run (superclass) @superclass (#match? @superclass "(ApplicationSystemTestCase)$")
+  ) @_ruby-test
+  (#set! tag ruby-test)
 )
 
 ; Examples
 (
-    (call
-       method: (identifier) @run (#any-of? @run "describe" "context" "it" "its" "specify")
-       arguments: (argument_list . (_) @_name)
-    ) @_ruby-test
-    (#set! tag ruby-test)
+  (call
+    method: (identifier) @run (#any-of? @run "describe" "context" "it" "its" "specify")
+      arguments: (argument_list . (_) @_name)
+  ) @_ruby-test
+  (#set! tag ruby-test)
 )
 
 ; Examples (one-liner syntax)
 (
-    (call
-        method: (identifier) @run (#any-of? @run "it" "its" "specify")
-        block: (_) @_name
-        !arguments
-    ) @_ruby-test
-    (#set! tag ruby-test)
+  (call
+    method: (identifier) @run (#any-of? @run "it" "its" "specify")
+    block: (_) @_name
+    !arguments
+  ) @_ruby-test
+  (#set! tag ruby-test)
 )
