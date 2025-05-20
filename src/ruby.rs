d80ec30e8e75b11adbb59c9d3c79dc0cf575d1c4
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2025-05-20 14:38:32 +0200

    Debugger prototype for Ruby
    This is a prototype implementation of Ruby debugger implementation, based on existing impl in core Zed. Input is most welcome on what it looks like and all that jazz.

--- src/ruby.rs
+++ src/ruby.rs
@@ -6,6 +6,18 @@ mod language_servers;
 use language_servers::{LanguageServer, Rubocop, RubyLsp, Solargraph, Sorbet, Steep};
 use zed_extension_api::{self as zed};
 
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
+use std::path::Path;
+
+use zed::lsp::{Completion, Symbol};
+use zed::settings::LspSettings;
+use zed::{serde_json, CodeLabel, LanguageServerId};
+use zed::{DebugAdapterBinary, DebugRequest, DebugTaskDefinition};
+use zed_extension_api::{
+    self as zed, resolve_tcp_template, Command, Result, StartDebuggingRequestArguments,
+    StartDebuggingRequestArgumentsRequest, TcpArgumentsTemplate, Worktree,
+};
+
 #[derive(Default)]
 struct RubyExtension {
     solargraph: Option<Solargraph>,
@@ -86,6 +98,79 @@ impl zed::Extension for RubyExtension {
             RubyLsp::SERVER_ID => self.ruby_lsp.as_ref()?.label_for_symbol(symbol),
             _ => None,
         }
+    }
+    fn get_dap_binary(
+        &mut self,
+        adapter_name: String,
+        config: DebugTaskDefinition,
+        _: Option<String>,
+        worktree: &Worktree,
+    ) -> Result<DebugAdapterBinary, String> {
+        let mut rdbg_path = Path::new(&adapter_name)
+            .join("rdbg")
+            .to_string_lossy()
+            .into_owned();
+
+        if worktree.which(&rdbg_path).is_none() {
+            match worktree.which("rdbg".as_ref()) {
+                Some(path) => rdbg_path = path.into(),
+                None => {
+                    let output = Command::new("gem")
+                        .arg("install")
+                        .arg("--no-document")
+                        .arg("--bindir")
+                        .arg(&adapter_name)
+                        .arg("debug")
+                        .output()?;
+                    if !output.status.is_none_or(|status| status != 0) {
+                        return Err(format!(
+                            "Failed to install rdbg:\n{}",
+                            String::from_utf8_lossy(&output.stderr).to_string()
+                        ));
+                    }
+                }
+            }
+        }
+
+        let tcp_connection =
+            config
+                .tcp_connection
+                .clone()
+                .unwrap_or_else(|| TcpArgumentsTemplate {
+                    port: None,
+                    host: None,
+                    timeout: None,
+                });
+        let connection = resolve_tcp_template(tcp_connection)?;
+        let DebugRequest::Launch(launch) = config.request.clone() else {
+            return Err("rdbg does not yet support attaching".to_string());
+        };
+
+        let mut arguments = vec![
+            "--open".to_string(),
+            format!("--port={}", connection.port),
+            format!("--host={}", connection.host),
+        ];
+        if worktree.which(launch.program.as_ref()).is_some() {
+            arguments.push("--command".to_string())
+        }
+        arguments.push(launch.program);
+        arguments.extend(launch.args);
+        let request = match config.request {
+            DebugRequest::Launch(_) => StartDebuggingRequestArgumentsRequest::Launch,
+            DebugRequest::Attach(_) => StartDebuggingRequestArgumentsRequest::Attach,
+        };
+        Ok(DebugAdapterBinary {
+            command: rdbg_path.to_string(),
+            arguments,
+            connection: Some(connection),
+            cwd: launch.cwd,
+            envs: launch.envs.into_iter().collect(),
+            request_args: StartDebuggingRequestArguments {
+                configuration: serde_json::Value::Object(Default::default()).to_string(),
+                request,
+            },
+        })
     }
 }
 
