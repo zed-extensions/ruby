7e00e8d59b5c946f3771952c7b559a10608e7a63
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2025-06-28 15:23:10 +0200

    feat(ruby): Add basic support for displaying dbg variables

---
+++ languages/ruby/debugger.scm
@@ -0,0 +1,13 @@
+(assignment left: (identifier) @debug-variable)
+(assignment left: (call receiver: (identifier) @debug-variable))
+(assignment left: (call method: (identifier) @debug-variable))
+(call (argument_list (call receiver: (identifier) @debug-variable)))
+(call (argument_list (call method: (identifier) @debug-variable)))
+
+(call (argument_list (identifier) @debug-variable))
+
+(method (method_parameters) @debug-variable)
+(body_statement (assignment (instance_variable) @debug-variable))
+
+(program) @debug-scope
+(body_statement) @debug-scope
