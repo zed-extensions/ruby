fa1acd197900cac2ad18eb23a2d288a6fee46090
AltCode <altcode@vivaldi.net>
2026-09-23 20:20:19 +0200

    fix(ruby): adjust specifity of word characters (#336)
    Only treat `!` and `?` as word characters when used in identifiers,
    symbols, hash key symbols, or the `defined?` method

--- tests/languages/ruby/overrides.rb
+++ tests/languages/ruby/overrides.rb
@@ -1,6 +1,10 @@
+# Overrides fixtures for tree-sitter queries
+
 # A Ruby comment
 plain_string = "plain text"
 interpolated_string = "hello #{user.name}"
 single_quoted_string = 'single quoted text'
 regexp = /[a-z]+/
 symbol = :valid?
+defined? function!
+{ symbol?: :"!delimited", "key-hash": "value", "string" => nil }
